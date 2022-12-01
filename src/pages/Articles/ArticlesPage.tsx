d436db57043d30813571f8382d2f926c26e8e53e
brvjeo <preobars@gmail.com>
2022-12-01 13:33:51 +0300

    feat: handle wrong routing, userID check

--- src/pages/Articles/ArticlesPage.tsx
+++ src/pages/Articles/ArticlesPage.tsx
@@ -3,11 +3,24 @@ import {Header} from '../../components/Header/Header';
 import React, {useEffect, useRef} from 'react';
 import {useNavigate, useParams} from 'react-router-dom';
 import {ArticlesSwiper} from '../../components/ArticlesSwiper/ArticlesSwiper';
-import styles from './ArticlesPage.module.scss';
 import {useAppSelector} from "../../store/hooks/useAppSelector";
 
 export const ArticlesPage: React.FC = (): React.ReactElement | null => {
+    const navigate = useNavigate();
+    const isMounted = useRef(false);
+    const {userID} = useParams();
     const {currentUser: user, viewed} = useAppSelector(state => state.user);
+
+    useEffect(
+        () => {
+            if(isMounted.current && userID !== user?.id){
+                navigate('/');
+            }else{
+                isMounted.current = true;
+            }
+        },
+        [isMounted.current]
+    )
 
     return (
         <Layout header={<Header/>}>
--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,8 +1,13 @@
 import {Panel} from '../../components/Panel/Panel';
 import {LoginForm} from '../../components/LoginForm/LoginForm';
 import styles from './LoginPage.module.scss';
+import {useAppSelector} from '../../store/hooks/useAppSelector';
+import {useContext, useEffect, useLayoutEffect, useRef} from 'react';
+import {ApplicationContext} from '../../App';
 
 export const LoginPage = () => {
+    const application = useContext(ApplicationContext);
+
     return (
         <div className={styles.container}>
             <Panel className={styles.panel}>
